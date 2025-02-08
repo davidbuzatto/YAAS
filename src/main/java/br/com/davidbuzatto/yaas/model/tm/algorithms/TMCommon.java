29dcacd6cc0fae040473e28a2c3697e3bae05068
David Buzatto <davidbuzatto@gmail.com>
2025-02-08 13:39:01 -0300

    Update TMCommon.java

--- src/main/java/br/com/davidbuzatto/yaas/model/tm/algorithms/TMCommon.java
+++ src/main/java/br/com/davidbuzatto/yaas/model/tm/algorithms/TMCommon.java
@@ -16,8 +16,8 @@
  */
 package br.com.davidbuzatto.yaas.model.tm.algorithms;
 
-import br.com.davidbuzatto.yaas.model.pda.PDA;
-import br.com.davidbuzatto.yaas.model.pda.PDAState;
+import br.com.davidbuzatto.yaas.model.tm.TM;
+import br.com.davidbuzatto.yaas.model.tm.TMState;
 import br.com.davidbuzatto.yaas.util.Utils;
 
 /**
@@ -31,77 +31,77 @@ public class TMCommon {
             Utils.getMavenModel().getProperties().getProperty( "debugAlgorithms" ) );
     
     /**
-     * Validades if a Pushdown Automaton have an initial state.
+     * Validades if a Turing Machine have an initial state.
      * 
-     * @param pda The Pushdown Automaton
-     * @throws IllegalArgumentException If the Pushdown Automaton don't have an
+     * @param tm The Turing Machine
+     * @throws IllegalArgumentException If the Turing Machine don't have an
      * initial state.
      */
-    public static void validateInitialState( PDA pda ) throws IllegalArgumentException {
+    public static void validateInitialState( TM tm ) throws IllegalArgumentException {
         
-        if ( pda.getInitialState() == null ) {
+        if ( tm.getInitialState() == null ) {
             throw new IllegalArgumentException( 
-                    "The Pushdown Automaton must have an initial state!" );
+                    "The Turing Machine must have an initial state!" );
         }
         
     }
     
     /**
-     * Validades if both Pushdown Automata have an initial state.
+     * Validades if both Turing Machines have an initial state.
      * 
-     * @param pda1 One Pushdown Automaton
-     * @param pda2 Another Pushdown Automaton
-     * @throws IllegalArgumentException If at least one of the Pushdown Automata
+     * @param tm1 One Turing Machine
+     * @param tm2 Another Turing Machine
+     * @throws IllegalArgumentException If at least one of the Turing Machine
      * don't have an initial state.
      */
-    public static void validateInitialState( PDA pda1, PDA pda2 ) throws IllegalArgumentException {
+    public static void validateInitialState( TM tm1, TM tm2 ) throws IllegalArgumentException {
         
-        if ( pda1.getInitialState() == null ) {
+        if ( tm1.getInitialState() == null ) {
             throw new IllegalArgumentException( 
-                    "The first Pushdown Automaton must have an initial state!" );
+                    "The first Turing Machine must have an initial state!" );
         }
         
-        if ( pda2.getInitialState() == null ) {
+        if ( tm2.getInitialState() == null ) {
             throw new IllegalArgumentException( 
-                    "The second Pushdown Automaton must have an initial state!" );
+                    "The second Turing Machine must have an initial state!" );
         }
         
     }
     
     /**
-     * Validades if a Pushdown Automaton have at least one final state.
+     * Validades if a Turing Machine have at least one final state.
      * 
-     * @param pda The Pushdown Automaton
-     * @throws IllegalArgumentException If the Pushdown Automata don't have at
+     * @param tm The Turing Machine
+     * @throws IllegalArgumentException If the Turing Machine don't have at
      * least one final state.
      */
-    public static void validateFinalStates( PDA pda ) throws IllegalArgumentException {
+    public static void validateFinalStates( TM tm ) throws IllegalArgumentException {
         
-        if ( pda.getFinalStates().isEmpty() ) {
+        if ( tm.getFinalStates().isEmpty() ) {
             throw new IllegalArgumentException( 
-                    "The Pushdown Automaton must have at least one final state!" );
+                    "The Turing Machine must have at least one final state!" );
         }
         
     }
     
     /**
-     * Validades if both Pushdown Automata have at least one final state.
+     * Validades if both Turing Machines have at least one final state.
      * 
-     * @param pda1 One Pushdown Automaton
-     * @param pda2 Another Pushdown Automaton
-     * @throws IllegalArgumentException If at least one of the Pushdown Automata
+     * @param tm1 One Turing Machine
+     * @param tm2 Another Turing Machine
+     * @throws IllegalArgumentException If at least one of the Turing Machine
      * don't have at least one final state.
      */
-    public static void validateFinalStates( PDA pda1, PDA pda2 ) throws IllegalArgumentException {
+    public static void validateFinalStates( TM tm1, TM tm2 ) throws IllegalArgumentException {
         
-        if ( pda1.getFinalStates().isEmpty() ) {
+        if ( tm1.getFinalStates().isEmpty() ) {
             throw new IllegalArgumentException( 
-                    "The first Pushdown Automaton must have at least one final state!" );
+                    "The first Turing Machine must have at least one final state!" );
         }
         
-        if ( pda2.getFinalStates().isEmpty() ) {
+        if ( tm2.getFinalStates().isEmpty() ) {
             throw new IllegalArgumentException( 
-                    "The second Pushdown Automaton must have at least one final state!" );
+                    "The second Turing Machine must have at least one final state!" );
         }
         
     }
@@ -111,12 +111,12 @@ public class TMCommon {
      * number 0. The other will reenumerate in appearance order inside the state
      * list.
      * 
-     * @param pda The Pushdown Automaon with the states to be processed.
+     * @param tm The Pushdown Automaon with the states to be processed.
      */
-    public static void reenumerateStates( PDA pda ) {
+    public static void reenumerateStates( TM tm ) {
         
         int currentState = 1;
-        for ( PDAState s : pda.getStates() ) {
+        for ( TMState s : tm.getStates() ) {
             if ( s.isInitial() ) {
                 s.setNumber( 0 );
             } else {
